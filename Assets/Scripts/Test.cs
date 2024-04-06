3e80c375f793db042cc034723909bd74347870e8
s_xgx1 <S_xgx1@outlook.com>
2024-04-06 10:30:33 +0800

    add

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,98 +1,68 @@
 using System;
-using Newtonsoft.Json;
-using OpenBLive.Runtime;
-using OpenBLive.Runtime.Data;
-using OpenBLive.Runtime.Utilities;
+using System.IO;
+using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using VYaml.Annotations;
+using VYaml.Emitter;
+using VYaml.Parser;
+using VYaml.Serialization;
+using Object = UnityEngine.Object;
+
+[Microsoft.CodeAnalysis.Generator]
+public class VYamlIncrementalSourceGenerator : IIncrementalGenerator
+{
+    
+}
+internal class UnityFormatter : IYamlFormatter<Object>
+{
+    public void Serialize(ref Utf8YamlEmitter emitter, Object value, YamlSerializationContext context)
+    {
+    }
+
+    public Object Deserialize(ref YamlParser parser, YamlDeserializationContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class X
+{
+    public int Value = 1;
+    public Vector3 Value2 = Vector3.up;
+}
 
 public class Test : MonoBehaviour
 {
-    private WebSocketBLiveClient _client;
-    private readonly string _accessKeySecret = "PKhF6NgLMKOZyMijyGD6bKhU";
-    private readonly string _accessKeyId = "FQ1us1hwUzIoZs1kGnUZW4qBkmpL9S";
-    private readonly string _code = "284815";
-    private long _appId;
-    private string _gameId;
-    private async void LinkStart(string code)
+    [SerializeField] private FontAsset font;
+
+    private async void Start()
     {
-        //测试的密钥
-        SignUtility.accessKeySecret = _accessKeySecret;
-        //测试的ID
-        SignUtility.accessKeyId = _accessKeyId;
-        //房间号Code，对应项目Id
-        var ret = await BApi.StartInteractivePlay(code, _appId.ToString());
-        //打印到控制台日志
-        var gameIdResObj = JsonConvert.DeserializeObject<AppStartInfo>(ret);
-        //处理开启游戏异常事件
-        if (gameIdResObj.Code != 0)
+        var x = new Asset
         {
-            Debug.LogError(gameIdResObj.Message);
-            return;
-        }
-
-        //连接长链
-        _client = new WebSocketBLiveClient(gameIdResObj.GetWssLink(), gameIdResObj.GetAuthBody());
-        _client.OnDanmaku += WebSocketBLiveClientOnDanmaku;
-        _client.OnGift += WebSocketBLiveClientOnGift;
-        _client.OnGuardBuy += WebSocketBLiveClientOnGuardBuy;
-        _client.OnSuperChat += WebSocketBLiveClientOnSuperChat;
-        try
-        {
-            //连接长链  需自己处理重连
-            _client.Connect();
-            //长链带有重试
-            // m_WebSocketBLiveClient.Connect(TimeSpan.FromSeconds(2),10);
-            Debug.Log("连接成功");
-        }
-        catch (Exception ex)
-        {
-            Debug.Log("连接失败");
-            throw;
-        }
-
-        //绑定游戏心跳
-        var gameId = gameIdResObj.GetGameId();
-        var beat = new InteractivePlayHeartBeat(gameId);
-        beat.HeartBeatError += json => { };
-        beat.HeartBeatSucceed += () => { };
-        beat.Start();
+            FontAsset = font
+        };
+        // //AssetDatabase.GetMainAssetTypeFromGUID()
+        // // var x = new X();
+        // // var json = JsonConvert.SerializeObject(x);
+        // var json = EditorJsonUtility.ToJson(x);
+        // await File.WriteAllTextAsync(Path.Combine(Application.dataPath, "temp.json"), json);
+        YamlSerializer.DefaultOptions.Resolver = CompositeResolver.Create(Array.Empty<IYamlFormatter>(),
+            new IYamlFormatterResolver[]
+            {
+                MainResolver.Instance,
+                StandardResolver.Instance
+            });
+        var yaml = YamlSerializer.SerializeToString(x);
+        var yamlPath = Path.Combine(Application.dataPath, "temp.yaml");
+        await File.WriteAllTextAsync(yamlPath, yaml);
+        await using var s = File.OpenRead(yamlPath);
+        var deserializeAsync = await YamlSerializer.DeserializeAsync<Asset>(s);
+        
     }
-
-    //绑定醒目留言
-    private void WebSocketBLiveClientOnSuperChat(SuperChat superChat)
-    {
-    }
-
-    //绑定大航海信息
-    private void WebSocketBLiveClientOnGuardBuy(Guard guard)
-    {
-    }
-
-    //绑定礼物信息
-    private void WebSocketBLiveClientOnGift(SendGift sendGift)
-    {
-    }
-
-    //绑定弹幕事件
-    private void WebSocketBLiveClientOnDanmaku(Dm dm)
-    {
-    }
-
-    private async void OpenProject()
-    {
-        //开启并获取项目的场次id，appId需要申请获得
-        var ret = await BApi.StartInteractivePlay(_code, _appId.ToString());
-        var retObject = JsonConvert.DeserializeObject<AppStartInfo>(ret);
-        if (retObject?.Code != 0)
-        {
-            return;
-        }
-
-        _gameId = retObject.Data?.GameInfo?.GameId;
-    }
-
-    private async void CloseProject()
-    {
-        var closeRes = await BApi.EndInteractivePlay(_appId.ToString(), _gameId);
-    }
+}
+[YamlObject]
+partial class Asset
+{
+    public FontAsset FontAsset;
 }
